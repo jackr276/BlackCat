ef15cd0448e6a65f84f63a951e32ce12e7292dfd
jackr276 <jack_m_robbins@me.com>
2023-12-27 13:55:21 -0500

    Created a map for character conversions

--- src/Crytpography.cpp
+++ src/Crytpography.cpp
@@ -5,14 +5,20 @@
 
 #include "Crytpography.h"
 
+
+
 //Using the LCG_RNG algorithm
 //taking in length, memory location of an array that will store our answer
 bool LCG_RNG(int length, int(&arr)[]){
     //set the seed
     srand(time(NULL));
     for(int i = 0; i < length; i++){
-        arr[i] = rand() % 256;
+        arr[i] = rand() % 82;
     }
+
     //TODO make the array a char array, convert numbers to ASCII
     return true;
 }
+
+
+
