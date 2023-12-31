a97bfaa528a7639c9d97859503007029ae8b4ffb
jackr276 <jack_m_robbins@me.com>
2023-12-30 19:52:58 -0500

    create determinant function

--- src/Matrix_Multiplication_Generator.h
+++ src/Matrix_Multiplication_Generator.h
@@ -10,6 +10,9 @@
 #include <random>
 
 class Matrix_Determinant_RNG {
+    private:
+        int matrix[3][3];
+
     //Constructor
     public:
         Matrix_Determinant_RNG(){
@@ -18,18 +21,27 @@ class Matrix_Determinant_RNG {
         }
 
         int getNextNumber(){
-            int matrix[3][3];
-
+            //rewrite the class's matrix every time
             for(int i = 0; i < 3; i++){
                 for (int j = 0; j < 3; j++){
-                    matrix[i][j] = rand();
+                    this->matrix[i][j] = rand();
                 }
             }
+
+            return matrix_determinant();
         }
 
     private:
-        int matrix_determinant(int (&matrix)[3][3]){
+        int matrix_determinant(){
             //will take in a 3-by-3 matrix and find determinant
+            //no need for fancy recursion here, use the formula for 3-by-3
+            int subdet1, subdet2, subdet3;
+
+            subdet1 = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2]*matrix[2][1]);
+            subdet2 = matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2]*matrix[2][0]);
+            subdet3 = matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1]*matrix[2][0]);
+            
+            return subdet1 + subdet2 + subdet3;
         }
 
 
